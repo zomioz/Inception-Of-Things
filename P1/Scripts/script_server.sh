74d22a4f23c6f4c2f47f5784102c99ef73f473c5
zomioz <pierrerulence@gmail.com>
2026-02-24 10:22:04 +0100

    FIX: uninstall libvirt and make all VM runing under virtualbox

--- P1/Scripts/script_server.sh
+++ P1/Scripts/script_server.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt install -y curl
-curl -sfL https://get.k3s.io | sh -s - server --token=tokentmp --write-kubeconfig-mode=644 --node-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - server --token=tokentmp --write-kubeconfig-mode=644 --node-ip=192.168.56.110 --advertise-address=192.168.56.110 --flannel-iface=eth1
--- P1/Scripts/script_serverworker.sh
+++ P1/Scripts/script_serverworker.sh
@@ -2,4 +2,6 @@
 
 apt-get update
 apt install -y curl
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=tokentmp sh -s - --node-ip=192.168.56.111
+curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=tokentmp sh -s - --node-ip=192.168.56.111 --flannel-iface=eth1
+systemctl enable k3s-agent
+systemctl start k3s-agent
