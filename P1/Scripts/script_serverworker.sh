d8ea602b4d59420fe676164acd6a749ec045bd6e
zomioz <pierrerulence@gmail.com>
2026-03-02 09:24:57 +0100

    REMOVE all to clear the branch

--- P1/Scripts/script_serverworker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt install -y curl
-curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=tokentmp sh -s - --node-ip=192.168.56.111 --flannel-iface=eth1
-systemctl enable k3s-agent
-systemctl start k3s-agent
