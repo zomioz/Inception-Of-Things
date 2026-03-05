ff068f67b9c628ee7f9d6902b32b8986299e80d5
iot <iot@iot.myguest.virtualbox.org>
2026-03-05 14:03:22 +0100

    FIX: SW IP

--- p1/scripts/install_serverworker.sh
+++ p1/scripts/install_serverworker.sh
@@ -2,9 +2,10 @@
 
 NODE_TOKEN=$1
 MASTER_IP="192.168.56.110"
+WORKER_IP="192.168.56.111"
 
 sudo apt-get update && sudo apt-get install -y curl
 
-curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_IP}:6443 K3S_TOKEN=${NODE_TOKEN} sh -s - agent --node-ip ${WORKER_IP}
 
-echo "Worker connected to Server"
+echo "Worker connected to Server with IP ${WORKER_IP}"
