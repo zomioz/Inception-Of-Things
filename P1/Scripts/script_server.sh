23ef28294b0c38c2f16be4e8c84b67be0f6326da
zomioz <pierrerulence@gmail.com>
2026-03-09 12:16:58 +0100

    CLEAR THE MAIN BRANCH

--- P1/Scripts/script_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt install -y curl
-export Token_tmp=$(cat /tmp/Token)
-curl -sfL https://get.k3s.io | sh -s - server --token=$Token_tmp --node-ip=192.168.56.110
-sleep 5
-echo "Server k3s installed"
--- P1/Scripts/script_serverworker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt install -y curl
-export Token_tmp=$(cat /tmp/Token)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$Token_tmp sh -s - --node-ip=192.168.56.111
