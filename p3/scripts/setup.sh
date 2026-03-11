f3f3415288281730c9d3f94ed520264097e1bc75
iot <iot@iot.myguest.virtualbox.org>
2026-03-11 14:14:32 +0100

    FIX: syntaxe error

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -7,8 +7,8 @@ k3d cluster create p3-cluster -p "8888:80@loadbalancer" --agents 2
 kubectl create namespace argocd
 kubectl create namespace dev
 
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 20
 kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
 
 kubectl apply -f p3/confs/application.yaml
