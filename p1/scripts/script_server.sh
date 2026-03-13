5c9e7b580932b7e94491d5bbd69149eb7f1336e9
zomioz <pierrerulence@gmail.com>
2026-03-13 12:17:59 +0100

    FIx: script_server to add sudo right vy default

--- p1/scripts/script_server.sh
+++ p1/scripts/script_server.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt install -y curl
 export Token_tmp=$(cat /tmp/Token)
-curl -sfL https://get.k3s.io | sh -s - server --token=$Token_tmp --node-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - server --token=$Token_tmp --node-ip=192.168.56.110 --write-kubeconfig-mode 644
 sleep 5
 echo "Server k3s installed"
